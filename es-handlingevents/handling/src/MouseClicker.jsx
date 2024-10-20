6e003a2fc877e791a513dcba29ee0bb01b9f37ea
acMarchetti <andreacosmamarchetti@yahoo.com>
2024-10-20 23:52:04 +0200

    post handling

---
+++ es-handlingevents/handling/src/MouseClicker.jsx
@@ -0,0 +1,20 @@
+export function MouseClicker() {
+  function handler(event) {
+    console.log(event.target.name);
+  }
+  function imgHandler(event) {
+    console.log(event.target.src);
+  }
+  return (
+    <>
+      <button name="one" onClick={handler}>
+        Click me!
+      </button>
+      <button name="two" onClick={handler}>
+        <img src="./gino" width={24} height={24} onClick={imgHandler} />
+        Click me!
+      </button>
+    </>
+  );
+}
+//the name of the button is not printed when the img is clicked because I used event.target and not event.currentTarget
