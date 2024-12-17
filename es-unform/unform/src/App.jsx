4cc8c1502fd9d73d8cac02f663ae5b91ad496fc9
acMarchetti <andreacosmamarchetti@yahoo.com>
2024-12-17 16:24:56 +0100

    ref2

--- es-unform/unform/src/App.jsx
+++ es-unform/unform/src/App.jsx
@@ -1,5 +1,7 @@
 import { FocusableInput } from "./FocusableInput";
 import UncontrolledLogin from "./UncrolledLogin";
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 
 export function App() {
   return (
