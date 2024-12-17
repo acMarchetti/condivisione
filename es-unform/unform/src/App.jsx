dcd1821500973ce0e0bd2f8f6b59f9b5b24b99a2
acMarchetti <andreacosmamarchetti@yahoo.com>
2024-12-17 16:31:15 +0100

    colors

--- es-unform/unform/src/App.jsx
+++ es-unform/unform/src/App.jsx
@@ -2,10 +2,25 @@ import { FocusableInput } from "./FocusableInput";
 import UncontrolledLogin from "./UncrolledLogin";
 import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
+import { Colors } from "./Colors";
+
+const colorList = [
+  { id: 1, name: "Red" },
+  { id: 2, name: "Blue" },
+  { id: 3, name: "Green" },
+  { id: 4, name: "Yellow" },
+  { id: 5, name: "Purple" },
+];
+
+export default App;
 
 export function App() {
   return (
     <StrictMode>
+      <div>
+        <h1>Colors List</h1>
+        <Colors colors={colorList} />
+      </div>
       <UncontrolledLogin />
       <FocusableInput />
     </StrictMode>
--- es-unform/unform/src/Colors.jsx
+++ es-unform/unform/src/Colors.jsx
@@ -1,10 +1,6 @@
-// Color Component: Receives a "color" object and displays its "name" property
-function Color({ color }) {
-  return <li>{color.name}</li>;
-}
+import { Color } from "./Color";
 
-// Colors Component: Receives an array of "color" objects and renders a list of Color components
-function Colors({ colors }) {
+export function Colors({ colors }) {
   return (
     <ul>
       {colors.map((color) => (
@@ -13,23 +9,3 @@ function Colors({ colors }) {
     </ul>
   );
 }
-
-// Example usage of the Colors component
-const colorList = [
-  { id: 1, name: "Red" },
-  { id: 2, name: "Blue" },
-  { id: 3, name: "Green" },
-  { id: 4, name: "Yellow" },
-  { id: 5, name: "Purple" },
-];
-
-function App() {
-  return (
-    <div>
-      <h1>Colors List</h1>
-      <Colors colors={colorList} />
-    </div>
-  );
-}
-
-export default App;
